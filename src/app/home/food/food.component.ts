9fa619c4911fdd1a982cb65fb087c78d199e74c8
raisusmita <susmitathulung53@gmail.com>
2020-09-05 15:49:27 +0545

    pagination in food related components

--- src/app/home/food/food.component.ts
+++ src/app/home/food/food.component.ts
@@ -32,40 +32,115 @@ export class FoodComponent implements OnInit {
   subFood: any[];
   foodHeader: any[];
 
+  pageSizeOptions = [10, 25, 50, 100];
+
+  pageSize: number;
+  pageIndex: number;
+  foodTotalLength: number;
+  mainFoodTotalLength: number;
+  subFoodTotalLength: number;
+  foodHeaderTotalLength: number;
+
+  limit: number;
+  skip: number;
+
+  tabLabel: string;
+
   @BlockUI() blockUI: NgBlockUI;
 
   constructor(private foodService: FoodService, private dialog: MatDialog) {}
 
   ngOnInit() {
+    this.pageSize = 10;
+    this.pageIndex = 0;
+    this.mainFoodTotalLength = 0;
+    this.foodTotalLength = 0;
+    this.subFoodTotalLength = 0;
+    this.foodHeaderTotalLength = 0;
+
+    this.skip = 0;
+    this.limit = this.pageSize;
+    this.initialize();
+  }
+
+  initialize() {
+    const paginationParams = {
+      limit: this.limit,
+      skip: this.skip,
+    };
+
     this.getFood();
     this.getMainFood();
     this.getSubFood();
     this.getFoodHeader();
   }
+  onPageChange(e: any) {
+    if (e.pageIndex === 0) {
+      this.skip = 0;
+    } else {
+      this.skip = e.pageIndex * e.pageSize;
+    }
+    this.limit = e.pageSize;
 
+    if (this.tabLabel == "Main Food") {
+      this.getMainFood();
+    } else if (this.tabLabel == "Sub Food") {
+      this.getSubFood();
+    } else if (this.tabLabel == "Food Header") {
+      this.getFoodHeader();
+    } else if (this.tabLabel == "Food Items") {
+      this.getFood();
+    }
+  }
   getFood() {
-    this.foodService.getFood().subscribe((data) => {
-      this.dataSource = data.data;
+    const paginationParams = {
+      limit: this.limit,
+      skip: this.skip,
+    };
+    this.foodService.getFoodList(paginationParams).subscribe((result) => {
+      this.dataSource = result.data;
+      this.foodTotalLength = result.totalCount;
     });
   }
 
+  onTabChange(e: any) {
+    this.tabLabel = e.tab["textLabel"];
+  }
+
   getMainFood() {
-    this.foodService.getMainFood().subscribe((data) => {
-      this.mainFood = data.data;
+    const paginationParams = {
+      limit: this.limit,
+      skip: this.skip,
+    };
+    this.foodService.getMainFoodList(paginationParams).subscribe((result) => {
+      this.mainFood = result.data;
+      this.mainFoodTotalLength = result.totalCount;
     });
   }
 
   getSubFood() {
-    this.foodService.getSubFood().subscribe((data) => {
-      this.subFood = data.data;
+    const paginationParams = {
+      limit: this.limit,
+      skip: this.skip,
+    };
+    this.foodService.getSubFoodList(paginationParams).subscribe((result) => {
+      this.subFoodTotalLength = result.totalCount;
+
+      this.subFood = result.data;
     });
   }
 
   getFoodHeader() {
     this.blockUI.start("Loading...");
-    this.foodService.getFoodHeader().subscribe(
+
+    const paginationParams = {
+      limit: this.limit,
+      skip: this.skip,
+    };
+    this.foodService.getFoodHeaderList(paginationParams).subscribe(
       (result) => {
         if (result && result.data) {
+          this.foodHeaderTotalLength = result.totalCount;
           this.foodHeader = result.data;
         } else {
           this.blockUI.stop();
