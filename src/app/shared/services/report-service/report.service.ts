6292df1626d6d5993e6f812fb18f13e0dd2e2161
raisusmita <susmitathulung53@gmail.com>
2021-02-16 18:54:40 +0545

    call API for Available Room Count

---
+++ src/app/shared/services/report-service/report.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { environment } from 'src/environments/environment'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportService {
+  private readonly baseURL = environment.apiURL
+  constructor(private http: HttpClient) {}
+
+  getAvailableRoomCount(): Observable<any> {
+    return this.http.get(this.baseURL + 'availableRoomCount')
+  }
+}
